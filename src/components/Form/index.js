6f857ae8e48d14d2a2c2b19ba4585f11bf0eb871
Adauto Júnior <dev.adautojunior@gmail.com>
2023-10-30 19:50:04 -0300

    feat: finished class 02

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,34 @@
 import './Form.css'
+import Button from '../Button';
 import TextField from '../TextField';
+import DropdownList from '../DropdownList';
 
 const Form = () => {
+  const teams = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'DevOps',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const onSave = (event) => {
+    event.preventDefault()
+
+    console.log('Form submetido')
+  }
+
   return (
     <section className='form'>
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField label='Nome' placeholder='Digite seu nome'/>
-        <TextField label='Cargo' placeholder='Digite seu cargo'/>
+        <TextField required={true} label='Nome' placeholder='Digite seu nome'/>
+        <TextField required={true} label='Cargo' placeholder='Digite seu cargo'/>
         <TextField label='Imagem' placeholder='Informe o endereço da imagem'/>
+        <DropdownList required={true} label='Time' items={teams}/>
+        <Button>Criar Card</Button>
       </form>
     </section>
   )
